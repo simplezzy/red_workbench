462abca2435099353b68ded75ce683826fe9441e
zhiyu.zhou <zhiyu.zhou@chinaredstar.com>
2017-06-28 10:50:48 +0800

    redis test

--- test-case/springboot-mybatis/src/test/java/com/redcode/workbench/springbootmybatis/service/UserServiceImplTest.java
+++ test-case/springboot-mybatis/src/test/java/com/redcode/workbench/springbootmybatis/service/UserServiceImplTest.java
@@ -1,15 +1,9 @@
 package com.redcode.workbench.springbootmybatis.service;
 
-import com.redcode.workbench.springbootmybatis.dao.UserMapper;
 import com.redcode.workbench.springbootmybatis.entity.User;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import utils.BaseTest;
-
-import static org.junit.Assert.*;
 
 public class UserServiceImplTest extends BaseTest {
 
